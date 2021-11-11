7845ef3ac242a79af398de5a7a54219cf3a2c086
Burburubunt <Buruburubunt@gmail.com>
2021-11-11 18:53:07 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    dots:true,
+    slidesToShow:1,
+    });   
+});
+
+$(document).ready(function(){
+    $('.client__slider').slick({
+    dots:true,
+    slidesToShow:3,
+    // arrows:true
+    });   
+});
+
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+    dots:true,
+    slidesToShow:1,
+    });   
+});
